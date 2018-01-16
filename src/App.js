59d325f607cd076b45a3b3a1ef10a5ab2a2478e7
Daniel Fosco <danielfosco@gmail.com>
2018-01-17 00:01:43 +0100

    Update JSON

--- src/App.js
+++ src/App.js
@@ -23,15 +23,6 @@ class App extends Component {
             </header>
             <div className="vh-80 flex justify-start items-start">
               <StationList />
-
-              <section className="bg-near-black pa2">
-                <div className="h-line nl2 nr2 bg-white"></div>
-                <span className="dib white helvetica f2 fw5 mb1">Van Siclen Av</span>
-                <div className="w-100">
-                  <span className="dib white helvetica bg-brown br-pill pa2 h1 w1 tc lh-solid">J</span>
-                  <span className="dib white helvetica bg-brown br-pill pa2 h1 w1 tc lh-solid">Z</span>
-                </div>
-              </section>
             </div>
             <footer className="vh-10 lh-copy helvetica f6 fw5 flex flex-row-ns flex-column justify-between">
               <p className="db ma0 w-100 w-50-ns mb3 mb0-ns">Inspired by <a className="link no-underline blue" target="_blank" rel="noopener" href="https://www.kickstarter.com/projects/nycsubway/the-new-york-city-subway-468-stations-1-poster">The New York City Subway</a> poster <br className="dn db-ns"/> and the <a className="link no-underline blue" target="_blank" rel="noopener" href="https://standardsmanual.com/products/nyctacompactedition">NYC Transit Authority Graphics Standards Manual</a></p>
