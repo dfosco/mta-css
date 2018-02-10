c23ef29dce4141f716464e238af1eec7835ae814
Daniel Fosco <danielfosco@gmail.com>
2018-02-10 20:45:46 +0100

    Fixed mapping

--- src/components/StationList.js
+++ src/components/StationList.js
@@ -4,11 +4,34 @@ import data from '../data/stops.json'
 
 export default function StationList(props) {
 
+  const metroStations = [
+    {
+    	"name": "Astor Pl",
+    	"line": ["4", "6"]
+    }, {
+    	"name": "Canal St",
+    	"line": ["4", "6"]
+    }, {
+    	"name": "50th St",
+    	"line": ["1", "2"]
+    }
+  ]
+
+  const arrayToObject = (array, keyField) =>
+     array.reduce((obj, item) => {
+       obj[item[keyField]] = item
+       return obj
+     }, {})
+
+  const metro = arrayToObject(data, "name")
+  // console.log(metroObject["50th St"].line[0])
+
+
   return(
-    data.map(data => {
-      return (
-        <Station key={data.line} name={data.name} line={data.line} />
-      )
-    })
+      data.map(metro => {
+            return (
+              <Station key={metro.line} name={metro.name} line={metro.line} />
+            )
+          })
   )
 }
