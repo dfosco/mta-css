fd1dd01a6a62604048d278ea98fcc94b562b7f11
Daniel Fosco <danielfosco@gmail.com>
2018-02-15 20:36:31 +0100

    Optical correction of SVGs

--- src/components/StationLine.js
+++ src/components/StationLine.js
@@ -5,21 +5,31 @@ import arrayToObject from '../utils/arrayToObject.js'
 export default function StationLine(props) {
   const chips = arrayToObject(lines, "line")
 
+  let isNum = "6.835"
+
+  if (props.children.match(/^(2|3|5|6|7|8|9|J)$/)) {
+    isNum = "8"
+  } else if (props.children.match(/^(1|4|F|L|A|E|R|Z|S)$/)) {
+    isNum = "7.5"
+  } else if (props.children.match(/^(N)$/)) {
+    isNum = "6.5"
+  } else if (props.children.match(/^(Q)$/)) {
+    isNum = "6"
+  } else if (props.children.match(/^(M)$/)) {
+    isNum = "5"
+  } else if (props.children.match(/^(W)$/)) {
+    isNum = "4.5"
+  }
+
   return (
     lines.map(chips => {
-      let isNum = false
-      if (props.children === "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9") {
-        isNum = true
-      }
-
       if (chips.line === props.line) {
-        console.log("isNum: ", isNum)
         return (
           <React.Fragment>
             <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg">
               <g fill="none" fillRule="evenodd"><circle fill={chips.color} cx="13" cy="13" r="13"/>
                 <text fontFamily="Helvetica Neue" fontSize="18" fontWeight="400" fill={chips.letter}>
-                  <tspan x={ isNum ? "8" : "6.835"} y="19">{props.children}</tspan>
+                  <tspan x={ isNum } y="19">{props.children}</tspan>
                 </text>
               </g>
             </svg>
