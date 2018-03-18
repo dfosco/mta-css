255e37bf55ecc00f300582c7b5d5f0ffdf272a97
Daniel Fosco <danielfosco@gmail.com>
2018-03-18 19:50:26 +0100

    Add basic meta tags Add 'CSS' on header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Head } from 'react-static'
 import StationList from './components/StationList'
+import StationLine from './components/StationLine'
 import favicon32 from "./img/favicon-32x32.png"
 import favicon16 from "./img/favicon-16x16.png"
 import tachyons from 'tachyons'
@@ -13,13 +14,26 @@ class App extends Component {
         <Head>
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta property="og:title" content="NYC Subway CSS" />
+          <meta name="description" content="A love letter to the NYC subway in CSS" />
+          <meta property="og:image" content="path/to/image.jpg" />
+
           <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
           <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
-          <title>New York City Transit Authority</title>
+          <title>NYC Subway CSS</title>
         </Head>
         <div className="flex flex-column justify-between bg-white pa3 ph5-ns pv4-ns">
             <header className="justify-between mb3">
-              <h1 className="black fw6 f2 dib">New York City Subway</h1>
+              <div className="flex flex-wrap items-center">
+                <h1 className="black fw6 f2 dib mb0 mb3-l">
+                  New York City Subway
+                </h1>
+                <div className="flex items-center mt2 mb3 w-100 w-auto-l ml2-l mb0-l dib-l">
+                  <StationLine large line="4">C</StationLine>
+                  <StationLine large line="1">S</StationLine>
+                  <StationLine large line="A">S</StationLine>
+                </div>
+              </div>
               <div className="mt3 mb2 lh-copy f6 fw5 flex flex-row-ns flex-column items-end justify-between">
                 <p className="db ma0 w-100 w-50-ns mb3 mb0-ns">Inspired by <a className="link no-underline blue" target="_blank" rel="noopener noreferrer" href="https://www.kickstarter.com/projects/nycsubway/the-new-york-city-subway-468-stations-1-poster">The New York City Subway</a> poster <br className="dn db-ns"/> and the <a className="link no-underline blue" target="_blank" rel="noopener" href="https://standardsmanual.com/products/nyctacompactedition">NYC Transit Authority Graphics Standards Manual</a>.</p>
                 <p className="db ma0 w-100 w-50-ns mb3 mb0-ns tl tr-ns">
--- src/components/StationLine.js
+++ src/components/StationLine.js
@@ -5,22 +5,22 @@ import arrayToObject from '../utils/arrayToObject.js'
 export default function StationLine(props) {
   const chips = arrayToObject(lines, "line")
 
-  let isNum = "6.835"
+  let propX = "6.835"
 
   if (props.children.match(/^(2|3|5|6|7|8|9|J)$/)) {
-    isNum = "8"
+    propX = "8"
   } else if (props.children.match(/^(1|4|F|L|A|E|R|Z|S)$/)) {
-    isNum = "7.5"
+    propX = "7.5"
   } else if (props.children.match(/^(R)$/)) {
-    isNum = "7"
+    propX = "7"
   } else if (props.children.match(/^(N)$/)) {
-    isNum = "6.5"
+    propX = "6.5"
   } else if (props.children.match(/^(Q)$/)) {
-    isNum = "6"
+    propX = "6"
   } else if (props.children.match(/^(M)$/)) {
-    isNum = "5"
+    propX = "5"
   } else if (props.children.match(/^(W)$/)) {
-    isNum = "4.5"
+    propX = "4.5"
   }
 
   return (
@@ -28,10 +28,10 @@ export default function StationLine(props) {
       if (chips.line === props.line) {
         return (
           <React.Fragment>
-            <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg">
+            <svg className={ props.large ? 'scale-1-4 pl-6px pt-6px' : '' } width="32" height="32" xmlns="http://www.w3.org/2000/svg">
               <g fill="none" fillRule="evenodd"><circle fill={chips.color} cx="13" cy="13" r="13"/>
-                <text fontFamily="Helvetica" fontSize="18" fontWeight="400" fill={chips.letter}>
-                  <tspan x={ isNum } y="19">{props.children}</tspan>
+                <text fontFamily="Helvetica" fontSize={ props.large ? '16' : '18'} fontWeight="400" fill={chips.letter}>
+                  <tspan x={ propX } y="19">{props.children}</tspan>
                 </text>
               </g>
             </svg>
