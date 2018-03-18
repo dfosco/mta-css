feaa9644e6135bf23a8a33479416817cd6bbb849
Daniel Fosco <danielfosco@gmail.com>
2018-03-18 20:23:14 +0100

    Add footer

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta property="og:title" content="NYC Subway CSS" />
-          <meta name="description" content="A love letter to the NYC subway in CSS" />
+          <meta name="description" content="A love letter to the NYC Subway in CSS" />
           <meta property="og:image" content="path/to/image.jpg" />
 
           <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
@@ -37,13 +37,16 @@ class App extends Component {
               <div className="mt3 mb0 lh-copy f6 fw5 flex flex-wrap flex-row-ns flex-column items-end justify-between">
                 <p className="db ma0 w-100 w-60-l mb3 mb0-l">Inspired by <a className="link no-underline blue" target="_blank" rel="noopener noreferrer" href="https://www.kickstarter.com/projects/nycsubway/the-new-york-city-subway-468-stations-1-poster">The New York City Subway</a> poster <br className="dn db-ns"/> and the <a className="link no-underline blue" target="_blank" rel="noopener" href="https://standardsmanual.com/products/nyctacompactedition">NYC Transit Authority Graphics Standards Manual</a>.</p>
                 <p className="db ma0 w-100 w-40-l mb0 tl tr-l">
-                  A love letter to the NYC transportation system, by <a href="https://twitter.com/dfosco" target="_blank" rel="noopener noreferrer" className="link no-underline blue">@dfosco</a>.
+                  A love letter to the NYC subway and its signs, by <a href="https://twitter.com/dfosco" target="_blank" rel="noopener noreferrer" className="link no-underline blue">@dfosco</a>.
                 </p>
               </div>
             </header>
             <main className="flex flex-wrap justify-start items-start na1">
               <StationList />
             </main>
+            <footer className="w-100">
+              <p className="db mt4 mb0 tc helvetica f6 fw7 near-black">No rights reserved :P</p>
+            </footer>
         </div>
       </React.Fragment>
     )
